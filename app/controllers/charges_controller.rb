09ac681306b2b84cc19030fbfb9359f7ace1915a
Steve Longoria <stevehl25@gmail.com>
2017-06-14 16:51:02 -0700

    Made price dynamic instead of hard coded

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,24 +1,25 @@
 class ChargesController < ApplicationController
+  before_action :amount_to_be_charged
+
   def new
-	end
+  end
 
-def create
-  @amount = 500
+  def create
+    customer = StripeTool.create_customer(email: params[:stripeEmail], 
+                                          stripe_token: params[:stripeToken])
 
-  customer = Stripe::Customer.create(
-    email: params[:stripeEmail],
-    source: params[:stripeToken]
-  )
+    charge = StripeTool.create_charge(customer_id: customer.id, 
+                                      amount: @amount,
+                                      description: 'Rails Stripe customer')
 
-  charge = Stripe::Charge.create(
-    customer: customer.id,
-    amount: @amount,
-    description: 'Rails Stripe customer',
-    currency: 'usd'
-  )
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_charge_path
+  end
 
-rescue Stripe::CardError => e
-  flash[:error] = e.message
-  redirect_to new_charge_path
-	end
+  private
+
+    def amount_to_be_charged
+      @amount = 500
+    end
 end
