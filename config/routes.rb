030141d1bdd9009837c7f1a80e3532a1ce2d6915
Steve Longoria <stevehl25@gmail.com>
2017-06-20 21:38:48 -0700

    Added contact form mailer file

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,10 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
-  resources :charges, only: [:new, :create]
+  resources :charges, only: :create
   get 'privacy', to: 'pages#privacy'
   get 'thanks', to: 'charges#thanks', as: 'thanks'
   resources :contacts, only: :create
 	get 'contact-us', to: 'contacts#new', as: 'new_contact'
+	get 'checkout', to: 'charges#new', as: 'new_charge'
 end
