96151d87eda0d01b0da791c67593952a02ee6a15
Steve Longoria <stevehl25@gmail.com>
2017-07-02 12:22:30 -0700

    Created payments controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'charges/new'
-
-  get 'charges/create'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'pages#home'
+	root to: 'pages#home'
+  resources :payments, only: [:new, :create]
+  get 'payment-thanks', to: 'payments#thanks', as: 'payment_thanks'
   resources :charges, only: :create
   get 'privacy', to: 'pages#privacy'
   get 'thanks', to: 'charges#thanks', as: 'thanks'
