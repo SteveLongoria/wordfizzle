89791ceb27933cf161c92e89bc2ad282d413c98b
Steve Longoria <stevehl25@gmail.com>
2017-07-09 15:30:19 -0700

    Added thank you page and download pdf link

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :charges, only: :create
   get 'privacy', to: 'pages#privacy'
-  get 'thanks', to: 'charges#thanks', as: 'thanks'
+  get 'ty-cust928382', to: 'charges#thanks', as: 'thanks'
   resources :contacts, only: :create
 	get 'contact-us', to: 'contacts#new', as: 'new_contact'
 	get 'checkout', to: 'charges#new', as: 'new_charge'
